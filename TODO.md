6c274a966b079c2804939cc80b1ac61ce33712f8
kgryte <kgryte@gmail.com>
2015-05-19 22:03:21 -0800

    [UPDATE] split methods into sep files. Initial poc.

--- TODO.md
+++ TODO.md
@@ -1,4 +1,11 @@
 TODO
 ====
 
-
+1. Create custom constructors based on `dtype` and `ndims`
+	-	use `new this.constructor`
+	-	file: `ctor.js`
+	-	these basically provide shortcuts for internal methods
+2. `order` (???)
+	-	name?
+3. cache methods which create other methods based on `ndims`
+4. 
--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,18 @@
 'use strict';
 
-var foo = require( './../lib' );
+var ndarray = require( 'ndarray' );
+
+var arr = ndarray( new Int8Array(128*128), [128,128] );
+
+var generator = require( './../lib/set.js' );
+
+var d = [ 1, 2, 3, 4, 5, 6 ];
+
+var createSetter = generator( 2 );
+console.log( createSetter.toString() );
+
+var set = createSetter( d, 0, [3,1] );
+console.log( set.toString() );
+
+set( 1, 2, 299 );
+console.log( d );
